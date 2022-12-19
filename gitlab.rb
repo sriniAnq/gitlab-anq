985949c5cec89ff92d8e22477b474e197b36e9dd
sriniAnq <109271384+sriniAnq@users.noreply.github.com>
2022-12-19 21:58:37 +0530

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -12,11 +12,17 @@ postgresql['enable'] = false
 gitlab_rails['db_adapter'] = 'postgresql'
 gitlab_rails['db_encoding'] = 'utf8'
 # gitlab_rails['db_host'] = 'anq-ci-dev.postgres.database.azure.com'
-gitlab_rails['db_host'] = 'anq-ci-dev.anq.dev'
+# gitlab_rails['db_host'] = 'anq-ci-dev.anq.dev'
+# gitlab_rails['db_port'] = '5432'
+# gitlab_rails['db_username'] = 'anq_mzxnvio'
+# gitlab_rails['db_password'] = '2fPE%&G4#gRR*4TeTg'
+
+gitlab_rails['db_host'] = 'postgresdb'
 gitlab_rails['db_port'] = '5432'
-gitlab_rails['db_username'] = 'anq_mzxnvio'
-gitlab_rails['db_password'] = '2fPE%&G4#gRR*4TeTg'
+gitlab_rails['db_username'] = 'postgres'
+gitlab_rails['db_password'] = 'postgrespw'
+
 
 # External URL and root password configuration
-external_url 'https://gitlab.anq.dev:8008'
+external_url 'localhost:8008'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
