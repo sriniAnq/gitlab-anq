4355f905cc6d48b5f5533a956f345fc167223143
sriniAnq <109271384+sriniAnq@users.noreply.github.com>
2022-12-30 15:05:44 +0530

    Update gitlab.rb

--- gitlab.rb
+++ gitlab.rb
@@ -6,22 +6,16 @@ nginx['listen_https'] = false
 letsencrypt['enable'] = false
 
 # Disable the built-in Postgres
-postgresql['enable'] = false
+# postgresql['enable'] = false
 
 # Fill in the connection details for database.yml
-gitlab_rails['db_adapter'] = 'postgresql'
-gitlab_rails['db_encoding'] = 'utf8'
+# gitlab_rails['db_adapter'] = 'postgresql'
+# gitlab_rails['db_encoding'] = 'utf8'
 # gitlab_rails['db_host'] = 'anq-ci-dev.postgres.database.azure.com'
 # gitlab_rails['db_host'] = 'anq-ci-dev.anq.dev'
 # gitlab_rails['db_port'] = '5432'
 # gitlab_rails['db_username'] = 'anq_mzxnvio'
 # gitlab_rails['db_password'] = '2fPE%&G4#gRR*4TeTg'
-
-gitlab_rails['db_host'] = 'postgresdb'
-gitlab_rails['db_port'] = '5432'
-gitlab_rails['db_username'] = 'postgres'
-gitlab_rails['db_password'] = 'postgrespw'
-
 
 # External URL and root password configuration
 external_url 'localhost:8008'
